b80d05c725fd1d2df0908f1c7b3e89cf7d6b419e
guibaumer <117869143+guibaumer@users.noreply.github.com>
2023-04-19 19:23:25 -0300

    daasdasD

--- 18-node/meuprojeto/public/modulos/validar.js
+++ 18-node/meuprojeto/public/modulos/validar.js
@@ -3,10 +3,12 @@
 class Login {
     constructor(formClass) {
         this.form = document.querySelector(formClass);
-        this.nome = document.querySelector('.nome').value;
-        this.sobrenome = document.querySelector('.sobrenome').value;
-        this.email = document.querySelector('.email').value;
+        this.nome = document.querySelector('.nome');
+        this.sobrenome = document.querySelector('.sobrenome');
+        this.email = document.querySelector('.email');
         this.emailBoolean = this.checaEmail(this.email);
+        this.divRes = document.querySelector('.res');
+        this.divRes.innerHTML = '';
         this.erros = [];
         this.prevenir()
     }
@@ -18,34 +20,60 @@ class Login {
         this.validar()
     }
     validar() {
-        if(!this.nome || this.nome.length > 20) {
-            this.erros.push('Nome inválido');
+        let filter_nome = /^([a-zA-Zà-úÀ-Ú]|\s+)+$/;
+
+        if(!this.nome.value || this.nome.value.length > 20) {
+            let erro = 'Nome inválido';
+            this.erros.push(erro);
+            this.mostraErro(this.nome);
         }
-        if(!this.sobrenome || this.sobrenome.length > 20) {
+        if(this.nome.value && !filter_nome.test(this.nome.value)) {
+            this.erros.push('Nome só pode conter letras');
+            if(!this.nome.classList.contains('error-input')) {
+            this.mostraErro(this.nome);
+            }
+        }
+        if(!this.sobrenome.value || this.sobrenome.value.length > 20) {
             this.erros.push('Sobrenome inválido');
+            this.mostraErro(this.sobrenome);
         }
         if(!this.emailBoolean) {
             this.erros.push('Email inválido')
+            this.mostraErro(this.email);
         }
-        this.concluir()
+        this.concluir();
     }
     checaEmail(email) {
         let res = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-        return res.test(email);
+        return res.test(email.value);
     }
     mostraErro(campo) {
-        campo.classList.add('error-input')
-        ////////////////////////////////////////////AQUI////////////////////////
+        campo.classList.add('error-input');
+        campo.addEventListener('click', () => {
+            campo.classList.remove('error-input');
+        });
     }
     concluir() {
-        // console.log(this.erros)
+        if(this.erros.length > 0) {
+        const div = document.createElement('div');
         for(let erro of this.erros) {
-            console.log(erro)
+            const p = document.createElement('p');
+            p.innerText = erro;
+            div.appendChild(p);
         }
+        this.divRes.appendChild(div);
+        this.divRes.style.display = 'block'
+    } else {
+        console.log('TUDO CERTO');
+        this.form.submit();
+    }
     }
 }
 
 const botao = document.querySelector('.enviar');
-botao.addEventListener('click', () => {
+if(botao) {
+    botao.addEventListener('click', () => {
     const login = new Login('.form-contato');
 })
+}
+
---
+++ 19-api/app.js
@@ -0,0 +1,1 @@
+//  iniciou com npm init -y, entao instalei o eslint, npm i, npm i express
